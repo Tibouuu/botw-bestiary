993a653ee2d767a26d79d3fa7bb96585b6f8cf30
Michael <gaelle.balois@gmail.com>
2023-03-23 08:28:23 +0100

    page monstre

--- src/Monster.js
+++ src/Monster.js
@@ -1,17 +1,21 @@
 import { Card } from "react-bootstrap";
 import { useState } from "react";
 import { Container } from "react-bootstrap"
+import "./Monster.css"
 
 export default function Monster(props) {
     return (
         <Container>
-            <p>Coucou</p>
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '90vw' }}>
                 <Card.Img variant="top" src={props.img_url} />
                 <Card.Body>
                     <Card.Title>{props.name}</Card.Title>
-                    <Card.Subtitle>{props.species}, {props.type}, {props.color}</Card.Subtitle>
+                    <Card.Subtitle>description</Card.Subtitle>
                     <Card.Text>{props.description}</Card.Text>
+                    <Card.Subtitle>biome</Card.Subtitle>
+                    <Card.Text>{props.biome}</Card.Text>
+                    <Card.Subtitle>Matériaux récupérables</Card.Subtitle>
+                    <Card.Text>{props.loot}</Card.Text>
                 </Card.Body>
             </Card>
         </Container>
